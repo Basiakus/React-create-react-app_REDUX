c77a88277682d453bf878527f2e05e90922a13cd
Adam Basiak <basiakus@gmail.com>
2018-01-12 20:26:18 +0100

    in process

---
+++ src/actions.js
@@ -0,0 +1,46 @@
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const EDIT_COMMENT = 'EDIT_COMMENT';
+export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
+export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+import uuid from 'uuid';
+//kreatory akcji
+function addComment(text) { // kreator akcji dodawania komentarza
+    return {
+        type: ADD_COMMENT,
+        text,
+        id: uuid.v4()
+    };
+}
+
+function removeComment(id) { // kreator akcji usuwania komentarza
+	return {
+		type: REMOVE_COMMENT,
+		id
+	};
+}
+
+function editComment(id, text) { // kreator funkcji edycji
+	return {
+		type: EDIT_COMMENT,
+		text,
+		id
+	};
+}
+
+function thumbUp(votes, id) { //kreator funkcji +1
+	return {
+		type: THUMB_UP_COMMENT,
+		votes,
+		id
+	};
+}
+
+function thumbDown(votes, id) { //kreator funkcji -1
+	return {
+		type: THUMB_DOWN_COMMENT,
+		votes,
+		id
+	};
+}
+export {thumbDown, thumbUp, editComment, removeComment, addComment};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import addComment from './actions.js';
+import reducer from './reducer.js';
+const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);	
+store.dispatch(addComment('pierwszy komentarz'));
+store.dispatch(addComment('drugi komentarz'));
