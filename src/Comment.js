442db4d63e03490e4d32a77e98233c56eba0c779
Adam Basiak <basiakus@gmail.com>
2018-01-14 19:48:30 +0100

    ???

--- src/Comment.js
+++ src/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {thumbUpComment, thumbDownComment} from './actions.js';
 
 const Comment = ({text, votes, id, thumbUpComment, thumbDownComment}) => 
 	<li>{text} <span>votes: {votes}</span>
