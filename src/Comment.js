9b4ca8106630d593e580feb4f706e70ff6c5e936
Adam Basiak <basiakus@gmail.com>
2018-01-15 19:11:54 +0100

    add close symbols

--- src/Comment.js
+++ src/Comment.js
@@ -4,7 +4,7 @@ const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComme
 	<li>{text} <span>votes: {votes}</span>
 		<button onClick={() => thumbUpComment(id)}><span role="img" aria-label="symbol">&#x1F44D;</span></button>
 		<button onClick={() => thumbDownComment(id)}><span role="img" aria-label="symbol">&#128078;</span></button>
-		<button onClick={() => removeComment(id)}><span role="img" aria-label="symbol">x</span></button>
+		<button onClick={() => removeComment(id)}><span role="img" aria-label="symbol">&#10007;</span></button>
 	</li>;
 
 export default Comment;
